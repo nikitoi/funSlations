93b63cc43d47a767b6d11fff98ac8d1b6a7e8c63
Nika Chervova <nikitoi@mail.ru>
2021-03-26 14:35:10 +0300

    different languages

--- public/js/translator.js
+++ public/js/translator.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const translation = document.querySelector('.translation');
   const sayLoudButton = document.querySelector('#sayloud_button');
+  const synth = window.speechSynthesis;
 
   document.forms.translateForm?.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -21,10 +22,32 @@ document.addEventListener('DOMContentLoaded', function() {
   const instances = M.Carousel.init(elems, {dist: -50, shift: 0, numVisible: 7});
 
   sayLoudButton?.addEventListener('click', (e) => {
+    const title = document.querySelector('.welcome_title').innerText;
+    const lanquage = title.slice(11);
+    console.log(lanquage);
+    let voiceNum = pickLang(lanquage);
+    console.log(voiceNum);
     const textToSay = e.target.parentElement.querySelector('p').innerText;
-    console.log(textToSay);
-    speechSynthesis.speak(
-      new SpeechSynthesisUtterance(textToSay)
-    );
+    const voices = synth.getVoices();
+    console.log(voices);
+    const utterance = new SpeechSynthesisUtterance(textToSay);
+    utterance.voice = voices[voiceNum];
+    speechSynthesis.speak(utterance);
   })
+
+  function pickLang (str) {
+    if (str === 'russian-accent translator') {
+      return 63;
+    } else if (str === 'hodor translator') {
+      return 11;
+    } else if (str === 'minion translator') {
+      return 21;
+    } else if (str === 'dothraki translator') {
+      return 15;
+    } else if (str === 'doggo translator') {
+      return 14;
+    } else {
+      return 7;
+    }
+  }
 });
